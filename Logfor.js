d589fba570bb7807607105f4bf582f6734965344
Nafeeskhan5598 <136472664+Nafeeskhan5598@users.noreply.github.com>
2023-06-23 17:59:08 +0530

    Add files via upload

---
+++ Logfor.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../Style/Logo.css';
+
+
+
+function Logofor(props){
+
+    return(
+        <>
+                {/* <div className="mycard">Mycard No. {props.cardno}                </div> */}
+        </>
+    )
+}
+export default Logofor;
